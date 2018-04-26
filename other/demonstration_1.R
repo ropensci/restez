36fbb28ef9bc48f4c607dc74b4c6e99f5f7493d2
Dom Bennett <dominic.john.bennett@gmail.com>
2018-04-26 22:37:08 +0200

    first demo

---
+++ other/demonstration_1.R
@@ -0,0 +1,35 @@
+# LIBS
+library(restez)
+
+# SETUP
+fp <- file.path(getwd(), 'demo')
+dir.create(fp)
+set_restez_path(fp)
+download_genbank()
+create_database()
+
+# LOOK UP ACCESSIONS
+# 33553 - Sciuromorpha - squirrel-like things
+search_term <- 'txid33553[Organism:exp] AND COI [GENE]'
+search_object <- rentrez::entrez_search(db = 'nucleotide', term = search_term,
+                                        use_history = TRUE, retmax = 0)
+accessions <- rentrez::entrez_fetch(db = 'nucleotide',
+                                    web_history = search_object$web_history,
+                                    rettype = 'acc')
+accessions <- strsplit(x = accessions, split = '\\n')[[1]]
+accessions <- sub(pattern = '\\.[0-9]+', replacement = '', x = accessions)
+
+# FETCH
+coi_sequences <- get_fasta(id = accessions)
+# are all accessions in results?
+all(accessions %in% names(coi_sequences))
+# .... no
+accessions[!accessions %in% names(coi_sequences)]
+# NC* refers to RefSeq sequences and are not currently available through restez
+# The sequence however exists in GB under a different id which we can find like so
+smmry <- rentrez::entrez_summary(db = 'nucleotide', id = 'NC_027278')
+# This ID does exist in our results.
+smmry$assemblyacc %in% accessions
+
+# TAKE DOWN
+delete_database()
--- other/dev2.R
+++ other/dev2.R
@@ -8,8 +8,22 @@ create_demo_database(n = 100)
 
 id <- c('demo_1', 'demo_2')
 
+res <- rentrez::entrez_summary(db = 'nucleotide', id = 'S71333')
+rentrez:::parse_esummary()
 
-res <- restez:::query_sql(nm = 'organism', id = id)
-ors <- res[['organism']]
-names(ors) <- res[['accession']]
-ors
+db = 'nucleotide'
+id = 'S71333'
+web_history = NULL
+version = "2.0"
+always_return_list = FALSE
+retmode = 'json'
+config = NULL
+args <- c(list("esummary", db = db, config = config, retmode = retmode,
+               version = version), 'id' = id)
+
+identifiers <- rentrez:::id_or_webenv()
+identifiers <- id
+
+response <- do.call(rentrez:::make_entrez_query, args)
+cat(response)
+rentrez:::parse_response
