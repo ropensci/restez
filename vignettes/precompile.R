9d65ff83c09294424bc6bc800bfe3418bc7300b8
Dominic Bennett <dominic.john.bennett@gmail.com>
2020-01-07 09:26:52 +0100

    precompiling v5 as well

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,4 +1,4 @@
-# First four vignettes require downloaded rodent db, must be precompiled:
+# First five vignettes require downloaded rodent db, must be precompiled:
 
 # gen rodent db
 if (!dir.exists(file.path('rodents'))) {
@@ -7,7 +7,8 @@ if (!dir.exists(file.path('rodents'))) {
 
 # precompile
 library(knitr)
-vgnts <- c('1_rodents.Rmd', '2_search_and_fetch.Rmd', '3_parsing.Rmd', '4_phylotaR.Rmd')
+vgnts <- c('1_rodents.Rmd', '2_search_and_fetch.Rmd', '3_parsing.Rmd',
+           '4_phylotaR.Rmd', '5_tips_and_tricks.Rmd')
 for (vgnt in vgnts) {
   knit(paste0("vignettes/", vgnt, ".orig"), paste0("vignettes/", vgnt))
 }
