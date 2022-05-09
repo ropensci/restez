df46b3ac430e3ad07ec2c45ff22d9f306ce758c0
Jeroen Ooms <jeroenooms@gmail.com>
2022-05-09 12:34:50 +0200

    More cleanups

--- tests/testthat/test-biomartr-tools.R
+++ tests/testthat/test-biomartr-tools.R
@@ -14,9 +14,4 @@ test_that('check_connection() works', {
     expect_true(restez:::check_connection())
   )
 })
-test_that('custom_download() works', {
-  with_mock(
-    `downloader::download` = function(...) TRUE,
-    expect_null(restez:::custom_download())
-  )
-})
+
